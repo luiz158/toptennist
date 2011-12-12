4e89c1cd8d4a66ae9c98e88af1e2af275577ab7a
Robson Ximenes <robsonximenes@gmail.com>
2011-12-12 12:48:03 -0200

    Layout da tela de entrada com login.

--- src/main/java/com/robsonximenes/toptennist/persistence/UsuarioDAO.java
+++ src/main/java/com/robsonximenes/toptennist/persistence/UsuarioDAO.java
@@ -1,6 +1,9 @@
 package com.robsonximenes.toptennist.persistence;
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.persistence.Query;
 
 import org.slf4j.Logger;
 
@@ -11,11 +14,22 @@ import com.robsonximenes.toptennist.domain.Usuario;
 
 @PersistenceController
 public class UsuarioDAO extends JPACrud<Usuario, Long> {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	@SuppressWarnings("unused")
 	private Logger logger;
-	
+
+	public Usuario verificar(Usuario usuario) {
+		Query query = getEntityManager().createQuery("from Usuario u where u.login = :pLogin and u.senha = :pSenha");
+		query.setParameter("pLogin", usuario.getLogin());
+		query.setParameter("pSenha", usuario.getSenha());
+		List<Usuario> result = query.getResultList();		
+		usuario = null;
+		if(!result.isEmpty())
+			usuario = result.get(0);
+		return usuario;
+	}
+
 }
--- src/main/java/com/robsonximenes/toptennist/view/UsuarioMB.java
+++ src/main/java/com/robsonximenes/toptennist/view/UsuarioMB.java
@@ -3,24 +3,41 @@ package com.robsonximenes.toptennist.view;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
+import br.gov.frameworkdemoiselle.util.Faces;
 
 import com.robsonximenes.toptennist.domain.Endereco;
 import com.robsonximenes.toptennist.domain.Telefone;
 import com.robsonximenes.toptennist.domain.Usuario;
+import com.robsonximenes.toptennist.persistence.UsuarioDAO;
 
 @ViewController
 @SessionScoped
-public class UsarioMB extends AbstractEditPageBean<Usuario, Long> implements Serializable{
+public class UsuarioMB extends AbstractEditPageBean<Usuario, Long> implements Serializable{
 	
 	private static final long serialVersionUID = -5764771730331382996L;
 	
+	private Usuario logado = null;
+	
+	@Inject
+	private UsuarioDAO dao;
+	
+	private Boolean lembrar;
 
-	public UsarioMB() {
+	public UsuarioMB() {
+				
+	}
+
+	@PostConstruct
+	public void cargaInicial() {
 		setBean(new Usuario());
 		getBean().setEmail("robsonximenes@gmail.com");
 		getBean().setEndereco(new Endereco());
@@ -36,10 +53,32 @@ public class UsarioMB extends AbstractEditPageBean<Usuario, Long> implements Ser
 		Telefone tel2 = new Telefone();
 		tel2.setDdd(11);
 		tel2.setNumero(26135929L);
-		getBean().getTelefones().add(tel2);
+		getBean().getTelefones().add(tel2);		
+		dao.insert(getBean());
+		setBean(new Usuario());
+	}
+	
+	
+	public String logar() {
+		logado = dao.verificar(getBean());
+		if(logado==null) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Falha no login, verifique usuario e senha."));
+		}
+		return null;
+	}
+	
+	
+	public Usuario getLogado() {
+		return logado;
 	}
 
+
 	
+	public void setLogado(Usuario logado) {
+		this.logado = logado;
+	}
+
+
 	@Override
 	@Transactional
 	public String delete() {
@@ -60,5 +99,14 @@ public class UsarioMB extends AbstractEditPageBean<Usuario, Long> implements Ser
 
 	@Override
 	protected void handleLoad() {
+	}
+	
+	public Boolean getLembrar() {
+		return lembrar;
+	}
+
+	
+	public void setLembrar(Boolean lembrar) {
+		this.lembrar = lembrar;
 	}
 }
